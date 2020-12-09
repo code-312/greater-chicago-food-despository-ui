62e1d0270fec30c681e9a32738df990198a61c8a
egabrielse <39629114+egabrielse@users.noreply.github.com>
2020-12-08 21:08:49 -0600

    Added ZoomToBounds components (#5)

---
+++ src/mapbox/Util.js
@@ -0,0 +1,26 @@
+import bbox from '@turf/bbox';
+import {FlyToInterpolator, WebMercatorViewport} from 'react-map-gl';
+
+
+/**
+  * updateViewportToFitBounds
+  * 
+  * Using bbox (https://www.npmjs.com/package/@turf/bbox) creates a 
+  * MapBox viewport that bounds the given GeoJSON feature.
+  * 
+  * @param currentViewport => current viewport of a MapBox map
+  * @param feature => GeoJSON feature to be bounded
+  * @returns MapBox viewport that fits to the bounds of the feature passed.
+  */
+export const updateViewportToFitBounds = (currentViewport, feature) => {
+    const [minLng, minLat, maxLng, maxLat] = bbox(feature); 
+    const webMercatorViewport = new WebMercatorViewport(currentViewport);
+    const {longitude, latitude, zoom} = webMercatorViewport.fitBounds([[minLng, minLat], [maxLng, maxLat]], {padding: 40});
+    return {
+      longitude: longitude,
+      latitude: latitude,
+      zoom: zoom,
+      transitionInterpolator: new FlyToInterpolator(),
+      transitionDuration: 1000
+    };
+}
