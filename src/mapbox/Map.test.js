5df4b3bdeafbc97016e5f621053086a88ad299b6
Alex Rosenfeld <arosenfeld2003@mac.com>
2021-02-25 21:42:05 -0800

    add one passing test to mock mapbox and render without crashing

--- src/mapbox/Map.test.js
+++ src/mapbox/Map.test.js
@@ -1,9 +1,19 @@
 import React from 'react';
 import { render } from '../test-utils';
 import Map from './Map';
+/*
+  See this issue for discussion about mocking the mapbox-gl library :
+  https://github.com/visgl/react-map-gl/issues/210
+*/
 
-describe.skip("Map", () => {
-    test("renders without crashing", () => {
-        render(<Map/>);
-    })
+jest.mock('mapbox-gl/dist/mapbox-gl', () => ({
+  Map: () => ({})
+}));
+
+describe('MapGL test', () => {
+  it('renders without crashing', () => {
+    render(<Map/>);
+  })
 })
+
+
