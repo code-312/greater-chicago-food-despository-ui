48acb6861bfccc4b7fa33e276eb37b1dc3a09c8c
Alex Rosenfeld <arosenfeld2003@mac.com>
2021-03-03 15:07:06 -0800

    move right hand menu data into component

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import { countyFetch } from './redux/countyReducer';
 import { zipFetch } from './redux/zipReducer';
 
 import ZoomToBoundsMenu from './components/ZoomToBoundsMenu';
+import RightHandMenu from './components/RightHandMenu';
 import Map from './mapbox/Map'
 
 class App extends Component {
@@ -31,52 +32,13 @@ class App extends Component {
           {/*Column 2: MapBox map */}
           <div className="col pl-0 pr-0">
             <Map />
-        
-        
-            {/* Righthand Map */}
-            <div className="menu" id="right-menu">
-                <div id="right-menu-county">
-                    <h2>County Map</h2>
-                    <label htmlFor="overall-poverty">
-                        Overall Poverty
-                        <input type="checkbox"  id="overall-poverty" name="overall-poverty" className="toggle" defaultChecked></input>
-                    </label>
-
-                    <label htmlFor="child-poverty">
-                        Child Poverty
-                        <input type="checkbox"  id="child-poverty" name="child-poverty" className="toggle" defaultChecked></input>
-                    </label>
-
-                </div>
-                <div id="right-menu-zipcode">
-                    <h2>Zipcode Map</h2>
-                    <label htmlFor="group1">
-                        Group 1
-                        <input type="checkbox"  id="group1" name="group1" className="toggle"></input>
-                    </label>
-
-                    <label htmlFor="group2">
-                        Group 2
-                        <input type="checkbox"  id="group2" name="group2" className="toggle"></input>
-                    </label>
-
-                    <label htmlFor="group3">
-                        Group 3
-                        <input type="checkbox"  id="group3" name="group3" className="toggle"></input>
-                    </label>
-
-                    <label htmlFor="group4">
-                        Group 4
-                        <input type="checkbox"  id="group4" name="group4" className="toggle"></input>
-                    </label>
-
-                    <label htmlFor="group5">
-                        Group 5
-                        <input type="checkbox"  id="group5" name="group5" className="toggle"></input>
-                    </label>
-                </div>
-              </div>
           </div>
+
+          {/* Column 3: Right-hand menu */}
+          { /* <nav className="menu col-2 pl-0 pre-0">
+            <RightHandMenu />
+          </nav> */
+          }
         </div>
       </div>
     );
---
+++ src/components/RightHandMenu.jsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { updateVP } from '../redux/viewportReducer';
+import {updateViewportToFitBounds} from '../mapbox/Util';
+
+<div className="menu" id="right-menu">
+    <div id="right-menu-county">
+        <h2>County Map</h2>
+        <label htmlFor="overall-poverty">
+            Overall Poverty
+            <input type="checkbox"  id="overall-poverty" name="overall-poverty" className="toggle" defaultChecked></input>
+        </label>
+
+        <label htmlFor="child-poverty">
+            Child Poverty
+            <input type="checkbox"  id="child-poverty" name="child-poverty" className="toggle" defaultChecked></input>
+        </label>
+    </div>
+    <div id="right-menu-zipcode">
+        <h2>Zipcode Map</h2>
+        <label htmlFor="group1">
+            Group 1
+            <input type="checkbox"  id="group1" name="group1" className="toggle"></input>
+        </label>
+
+        <label htmlFor="group2">
+            Group 2
+            <input type="checkbox"  id="group2" name="group2" className="toggle"></input>
+        </label>
+
+        <label htmlFor="group3">
+            Group 3
+            <input type="checkbox"  id="group3" name="group3" className="toggle"></input>
+        </label>
+
+        <label htmlFor="group4">
+            Group 4
+            <input type="checkbox"  id="group4" name="group4" className="toggle"></input>
+        </label>
+
+        <label htmlFor="group5">
+            Group 5
+            <input type="checkbox"  id="group5" name="group5" className="toggle"></input>
+        </label>
+    </div>
+</div>
