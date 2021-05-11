5a93442c3a05d1a2d39125f0a9d62ed0375c9bfd
maharit108 <maha.rit108@gmail.com>
2021-05-10 19:23:30 -0500

    Wrapper positioning and few element added

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import { zipFetch } from './redux/zipReducer';
 import { zipDataFetch } from './redux/zipCodeDataReducer';
 
 import ZoomToBoundsMenu from './components/ZoomToBoundsMenu';
-import RightHandMenu from './components/RightHandMenu';
+import RightHandMenu from './components/RightHandMenu/RightHandMenu';
 import Map from './mapbox/Map'
 
 const App = () => {
@@ -22,23 +22,24 @@ const App = () => {
   }, [])
 
     return (
-      <div className="container-fluid">
-        <div className="row">
+      <div className="main">
+        <div className="container-fluid">
+          <div className="row">
 
-          {/*Column 1: Left-hand menu (Zoom Control)*/}
-          <nav className="menu col-2 pl-0 pr-0">
-            <ZoomToBoundsMenu /> 
-          </nav>
+            {/*Column 1: Left-hand menu (Zoom Control)*/}
+            <nav className="col-3 pl-0 pr-0 menu ">
+              <ZoomToBoundsMenu /> 
+            </nav>
 
-          {/*Column 2: MapBox map */}
-          <div className="col pl-0 pr-0">
-            <Map />
+            {/*Column 2: MapBox map */}
+            <div className="col-9 pl-0 pr-0">
+              <Map />
+            </div>
           </div>
-
-          {/* Column 3: Right-hand menu */}
-          <nav className="menu col-2 pl-0 pre-0">
-            <RightHandMenu />
-          </nav>
+        </div>
+        {/* Column 3: Right-hand menu */}
+        <div className="rtMenuWrapper">
+          <RightHandMenu />
         </div>
       </div>
     );
