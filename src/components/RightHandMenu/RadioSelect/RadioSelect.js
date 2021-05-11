642740f8e9cae5b707bf4397c6ae7c2f4dddd829
maharit108 <maha.rit108@gmail.com>
2021-05-10 23:23:35 -0500

    radioOption Styling complete

--- src/components/RightHandMenu/RadioSelect/RadioSelect.js
+++ src/components/RightHandMenu/RadioSelect/RadioSelect.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import './RadioSelect.css'
 
@@ -8,12 +8,17 @@ const options = {
 }
 
 function RadioSelect(props) {
+  const [selectFeat, setFeat] = useState(0)
+  const radioClick = (idx) => {
+    setFeat(idx)
+  }
+
   return (
     <div className='radioSel'>
         {options[props.data].map((feature, idx) => (
             <label htmlFor={feature} key={idx}>
-              <input type="radio" id={feature} name={feature} className='radioOpt'></input>
-              {feature}
+              <input type="radio" id={feature} name={feature} className='radioOpt' checked={selectFeat===idx} onClick={() => radioClick(idx)}></input>
+              <h3 className='radioLabel'>{feature}</h3>
             </label>
         ))}
     </div>
