66491a4cfc5a886cfec1875a1861bda11c493c76
Magnus Frennberg <mfrennberg@hotmail.com>
2021-05-12 16:37:45 -0500

    Good padding and labels

--- src/components/DatasetSelector.js
+++ src/components/DatasetSelector.js
@@ -7,27 +7,31 @@ function DatasetSelector(props) {
 	);
 
 	const handleSelection = (e) => {
-        const indexOfSelection = options.indexOf(e.target.id)
+		const indexOfSelection = options.indexOf(e.target.id);
 		setSelectedOptions(options[indexOfSelection]);
 	};
 
-    /* For later - When state changes, update the store */
-    // const onSelectChange = useEffect() ... 
+	/* For later - When state changes, update the store */
+	// const onSelectChange = useEffect() ...
 
 	/* Don't load this bar if there are no dataset options */
 	return !options ? null : (
 		<div id="data-selector">
-			{options.map((option, i) => {
-				const selected = selectedOption === option;
-				return (
-					<DatasetOption
-						option={option}
-						key={i}
-						selected={selected}
-						handleSelection={handleSelection}
-					/>
-				);
-			})}
+			<h3>Show data for:</h3>
+			<ul className="flex-list">
+				{options.map((option, i) => {
+					const selected = selectedOption === option;
+					return (
+						<li key={i}>
+							<DatasetOption
+								option={option}
+								selected={selected}
+								handleSelection={handleSelection}
+							/>
+						</li>
+					);
+				})}
+			</ul>
 		</div>
 	);
 }
@@ -44,8 +48,11 @@ function DatasetOption(props) {
 				id={props.option}
 				onChange={handleChange}
 				checked={props.selected}
-				data-index={props['data-index']}></input>
-			<label htmlFor={props.option}>{props.option}</label>
+				data-index={props['data-index']}
+				className="align-self-center"></input>
+			<label htmlFor={props.option} className="align-self-center">
+				{props.option}
+			</label>
 		</>
 	);
 }
