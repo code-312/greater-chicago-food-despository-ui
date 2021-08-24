daf46ac999ad8dc655cbfa0958d6718fadee0081
Khalfani Wadlington <khalfani@khalfaniw.com>
2021-08-24 13:34:42 -0500

    Create default selections for map

--- src/mapbox/CountyColorsUtil.js
+++ src/mapbox/CountyColorsUtil.js
@@ -72,34 +72,42 @@ export function retrieveCountyAndMetricDictionary(selectedFeat, extraFeat, count
    the Source component in the map. Example:
  <Source id="counties" type="geojson" data={illinois_counties.counties}>
 */
-  const { selectedfilterFeat, selectedfilterSubfeat } = selectedFeat;
-  const { selectedExtraDataFeat } = extraFeat;
+  let { selectedfilterFeat, selectedfilterSubfeat } = selectedFeat;
+  let { selectedExtraDataFeat } = extraFeat;
   let countyMetricDict = {};
 
+//Note that if we change te default race we have to update extra data menu
   if (countyData) {
     for (const county in countyData) {
       let countyName_Key = countyData[county].NAME.substring(0,countyData[county].NAME.length - 17);
       if (selectedfilterFeat) {
         switch (selectedfilterFeat) {
           case 'poverty_data':
+            selectedfilterSubfeat = selectedfilterSubfeat ||  "poverty_population_total"; 
             countyMetricDict[countyName_Key] = countyData[county][selectedfilterFeat][selectedfilterSubfeat];
             break;
 
           case 'insecurity_data':
+            selectedfilterSubfeat = selectedfilterSubfeat ||  "insecurity_2018_child"; 
             countyMetricDict[countyName_Key] = countyData[county][selectedfilterFeat][selectedfilterSubfeat];
             break;
           
           case 'race_data':
+            selectedExtraDataFeat = selectedExtraDataFeat ||  "race_asian"; 
             countyMetricDict[countyName_Key] = countyData[county][selectedfilterFeat][selectedExtraDataFeat];
             break;
           
           case 'snap_data':
+            selectedfilterSubfeat = selectedfilterSubfeat ||  "2019age_18-65"; 
+            selectedExtraDataFeat = selectedExtraDataFeat ||  "race_asian"; 
             if (selectedfilterSubfeat && selectedfilterSubfeat.length > 4) {
               countyMetricDict[countyName_Key] = countyData[county][selectedfilterFeat][selectedfilterSubfeat.substring(0,4)][selectedfilterSubfeat.substring(4)][selectedExtraDataFeat];
             };
             break;
           
           case 'WIC':
+            selectedfilterSubfeat = selectedfilterSubfeat ||  "wic_participation_total_data"; 
+            selectedExtraDataFeat = selectedExtraDataFeat ||  "race_asian"; 
             if (selectedfilterSubfeat && selectedExtraDataFeat) {
               if (selectedfilterSubfeat.substring(0,3) === 'wic' && selectedExtraDataFeat.substring !== 'wic') {
                 if (countyData[county][selectedfilterSubfeat]) {
