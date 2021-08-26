d3b9b163b4902475c95e861808eea93cc4effcdd
kylefarmer85 <kylefarmer85@gmail.com>
2021-08-26 18:50:53 -0500

    remove styling for 4 lines max and display all text on focus

--- src/components/RightHandMenu/RightHandMenu.js
+++ src/components/RightHandMenu/RightHandMenu.js
@@ -50,7 +50,7 @@ const RightHandMenu = () => {
       <div className='rtMenu'>
         <div className='rtBody'>
           <h1 className='rt__title'>{dataOptions[selectedfilterFeat].title}</h1>
-          <p className='rt__desc' tabindex='0'>{dataOptions[selectedfilterFeat].desc}</p>
+          <p className='rt__desc'>{dataOptions[selectedfilterFeat].desc}</p>
           <h3 className='rt__name'>{selectedCounty ? selectedCounty.name + ' County' : ''}</h3>
 
           { dataOptions[selectedfilterFeat].toggleSelect ? (
